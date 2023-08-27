d86414d9ca8d0f6563647267617a52acc835d7bf
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-08-27 22:01:54 +0630

    first commit

---
+++ 1_vue_composition_api_and_pinia/src/components/Counter.vue
@@ -0,0 +1,24 @@
+<template>
+    <div class="d-flex align-items-center justify-content-center">
+        <button @click.prevent="decreaseCount" type="button" class="btn btn-secondary">-</button>
+        <span>{{ count }}</span>
+        <button @click.prevent="increaseCount" type="button" class="btn btn-primary">+</button>
+    </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const count = ref(0);
+
+const decreaseCount = () => {
+    if(count.value !== 0) {
+        count.value--;
+    }
+}
+const increaseCount = () => {
+    if(count.value > -1) {
+        count.value++;
+    }
+}
+</script>
