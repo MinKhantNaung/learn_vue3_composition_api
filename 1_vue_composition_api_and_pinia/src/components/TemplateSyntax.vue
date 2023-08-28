8c86c982166e56c420d4f837e7888a5b5970ddf7
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-08-28 21:06:35 +0630

    template syntax

---
+++ 1_vue_composition_api_and_pinia/src/components/TemplateSyntax.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <h1 :id="dynamicId" :[titleAttr]="titleValue">Hello Template</h1> 
+        <h2 v-html="message"></h2>
+        <div v-bind="objectOfIdClass"></div>
+        <div>{{ 1 + 1 }}</div>
+        <span @click="showAlert">{{ message ? 'yes' : 'no' }}</span>
+        <span @[eventName].prevent="clicked">{{ Math.random() }}</span>
+    </div>
+</template>
+
+<script setup>
+const message = 'Hello message';
+const dynamicId = 'my-id';
+const objectOfIdClass= {
+    id: dynamicId,
+    class: 'container',
+};
+const titleAttr = 'title';
+const titleValue = 'This is title';
+const showAlert = () => alert('show alert');
+const eventName = 'mouseover';
+const clicked = () => alert('clicked');
+</script>
--- 1_vue_composition_api_and_pinia/src/views/HomeView.vue
+++ 1_vue_composition_api_and_pinia/src/views/HomeView.vue
@@ -1,11 +1,12 @@
 <script setup>
 import Counter from '../components/Counter.vue';
-
+import TemplateSyntax from '../components/TemplateSyntax.vue';
 
 </script>
 
 <template>
   <div>
     <Counter/>
+    <TemplateSyntax />
   </div>
 </template>
