eea045ec9223d41a58174652f0ffd5547aa9c9b7
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-08-28 21:23:55 +0630

    reactive

--- 1_vue_composition_api_and_pinia/src/components/Counter.vue
+++ 1_vue_composition_api_and_pinia/src/components/Counter.vue
@@ -1,15 +1,33 @@
 <template>
-    <div class="d-flex align-items-center justify-content-center">
+    <div class="text-center">
+        <h3>{{ counterTitle }}</h3> <br>
         <button @click.prevent="decreaseCount" type="button" class="btn btn-secondary">-</button>
         <span>{{ count }}</span>
         <button @click.prevent="increaseCount" type="button" class="btn btn-primary">+</button>
+        <br>
+        <input type="text" v-model="counterTitle">
+        <br>
+        <br>
+        <h4>{{ counterData.title }}</h4>
+        <h5 @click="increaseReactiveCount">{{ counterData.count }}</h5>
     </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import { reactive, ref } from "vue";
 
 const count = ref(0);
+
+const counterTitle = ref('Counter Title');
+
+const counterData = reactive({
+    count: 0,
+    title: 'reactiveCounter'
+});
+
+const increaseReactiveCount = () => {
+    counterData.count++;
+}
 
 const decreaseCount = () => {
     if(count.value !== 0) {
