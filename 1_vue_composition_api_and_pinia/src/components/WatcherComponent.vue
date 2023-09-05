4a90b07df65ae4ceef7d39f09212c1a4a585fc7b
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-09-05 22:17:40 +0630

    watch source types in ref

--- 1_vue_composition_api_and_pinia/src/components/WatcherComponent.vue
+++ 1_vue_composition_api_and_pinia/src/components/WatcherComponent.vue
@@ -9,6 +9,17 @@
       <p>Answer: {{ answer.answer }}</p>
       <img :src="responseData.image" style="width: 200px" />
     </div>
+
+    <h2>Watch Source Types</h2>
+    <h3>Single Ref</h3>
+    x : <input v-model="x" type="text" />
+    <p>{{ changedX }}</p>
+    y : <input v-model="y" type="text" /> <br />
+    z : <input v-model="z" type="text" />
+    <h3>{{ changeXYZ }}</h3>
+    <br />
+    {{ changeXY }} <br />
+    sum is {{ total }}
   </div>
 </template>
 
@@ -33,5 +44,40 @@ watch(question, async (newQuestion, oldQuestion) => {
       answer.value = "Error! Could not reach the API";
     }
   }
+});
+
+// watch source types
+// single ref
+const x = ref(0);
+const y = ref(0);
+const z = ref(0);
+
+const changedX = ref(0);
+const changeXY = ref("");
+const changeXYZ = ref("");
+
+watch(x, (newX) => {
+  changedX.value = `new x is ${newX}`;
+});
+watch(y, (newY) => {
+  changedX.value = `new y is ${newY}`;
+});
+
+// Getter
+const total = ref(0);
+watch(
+  () => parseInt(x.value) - parseInt(y.value),
+  (sum) => {
+    total.value = sum;
+  }
+);
+
+// array of multiple sources
+watch([x, () => y.value], ([newX, newY]) => {
+  changeXY.value = `x is changed to ${newX} and y is changed to ${newY}`;
+});
+
+watch([x, () => y.value, () => z.value], ([newX, newY, newZ]) => {
+  changeXYZ.value = `x is ${newX}, y is ${newY} and z is ${newZ}`;
 });
 </script>
