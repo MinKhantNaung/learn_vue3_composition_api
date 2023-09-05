4fb903cb993a2ce15a4f0d45f2035ee993131c03
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-09-05 08:21:14 +0630

    Form input

---
+++ 1_vue_composition_api_and_pinia/src/components/FormInput.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <hr />
+    <h1>Form Input</h1>
+    <div>
+      <input
+        :value="text"
+        @input="(event) => (text = event.target.value)"
+        type="text"
+      />
+      <span>{{ text }}</span>
+      <p>Instead, we can use v-model.</p>
+      <input v-model="text" type="text" />
+      <span>{{ text }}</span> <br />
+      <textarea v-model="message"></textarea>
+      <span>{{ message }}</span>
+      <br />
+      <p>Checkbox</p>
+      <input v-model="checked" type="checkbox" />{{ checked }}
+      <p>Multi check</p>
+      <input v-model="checkedValues" type="checkbox" value="Laravel" />Laravel
+      <input v-model="checkedValues" type="checkbox" value="Vue" />Vue
+      <input v-model="checkedValues" type="checkbox" value="Nuxt" />Nuxt
+      <input v-model="checkedValues" type="checkbox" value="React" />React
+      <p>{{ checkedValues }}</p>
+      <p>Radio</p>
+      <input type="radio" value="Next" v-model="picked" />
+      <input type="radio" value="React Native" v-model="picked" />
+      <p>{{ picked }}</p>
+      <p>Select Box</p>
+      <select v-model="selectedValue">
+        <option value="">Select A value</option>
+        <option value="A">A</option>
+        <option value="B">B</option>
+        <option value="C">C</option>
+      </select>
+      <p>Select Value is : {{ selectedValue }}</p>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const text = ref("Nang");
+const message = ref("Hello");
+const checked = ref(false);
+const checkedValues = ref([]);
+const picked = ref("");
+const selectedValue = ref("");
+</script>
--- 1_vue_composition_api_and_pinia/src/views/HomeView.vue
+++ 1_vue_composition_api_and_pinia/src/views/HomeView.vue
@@ -1,6 +1,7 @@
 <script setup>
 import Counter from "../components/Counter.vue";
 import EventHandling from "../components/EventHandling.vue";
+import FormInput from "../components/FormInput.vue";
 import ListRendering from "../components/ListRendering.vue";
 import TemplateSyntax from "../components/TemplateSyntax.vue";
 </script>
@@ -15,5 +16,6 @@ import TemplateSyntax from "../components/TemplateSyntax.vue";
     <ConditionalRender />
     <ListRendering />
     <EventHandling />
+    <FormInput />
   </div>
 </template>
