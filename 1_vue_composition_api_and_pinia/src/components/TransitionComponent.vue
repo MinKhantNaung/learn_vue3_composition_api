8d1fc4dbd21140719ef078044e5ccdb14689fccf
Minkhantnaung@securelink <minkhantnaung@securelinkmm.com>
2023-09-30 15:33:37 +0630

    Transition Classes name transition (fade)

--- 1_vue_composition_api_and_pinia/src/components/TransitionComponent.vue
+++ 1_vue_composition_api_and_pinia/src/components/TransitionComponent.vue
@@ -3,7 +3,7 @@
         <hr>
         <h1>Transition Component</h1>
         <button @click.prevent="show = !show">Toggle</button>
-        <Transition>
+        <Transition name="fade">
             <h1 v-if="show" class="bg-success text-white">Min Khant Naung (Laravel Vue Nuxt)</h1>
         </Transition>
     </div>
@@ -15,11 +15,51 @@ const show = ref(true)
 </script>
 
 <style scoped>
-.v-enter-from, .v-leave-to {
+/* .v-enter-from, .v-leave-to {
     opacity: 0;
 }
 
 .v-enter-active, .v-leave-active {
     transition: opacity 0.5s ease;
+} */
+
+/* transition classes */
+/* .v-enter-from */
+/* .v-enter-to */
+/* .v-enter-active  */
+
+/* .v-leave-from */
+/* .v-leave-to */
+/* .v-leave-active  */
+/* transition classes */
+
+/* .v-enter-from {
+    opacity: 0;
+}
+
+.v-enter-to {
+    opacity: 1;
+}
+
+.v-leave-from {
+    opacity: 1;
+}
+
+.v-leave-to {
+    opacity: 0;
+}
+
+.v-enter-active, .v-leave-active {
+    transition: opacity 2s ease; */
+/* } */
+
+.fade-enter-active,
+.fade-leave-active {
+  transition: opacity 0.5s ease;
+}
+
+.fade-enter-from,
+.fade-leave-to {
+  opacity: 0;
 }
 </style>
